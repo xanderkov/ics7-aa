a2e9a88ede02f76efdaee899e0b54e449e4169c2
Kovel Alexadner <xanderkovel@gmail.com>
2022-09-05 21:52:24 +0300

    Added some tests

--- lab-01/levenstein/bench_test.go
+++ lab-01/levenstein/bench_test.go
@@ -2,6 +2,26 @@ package levenstein
 
 import "testing"
 
+func BenchmarkRecursiveLen10(b *testing.B) {
+	src := "abaoboaobj"
+	dest := "da;ldfjalj"
+
+	for i := 0; i < b.N; i++ {
+		CountLevNoRec(src, dest)
+	}
+
+}
+
+func BenchmarkRecursiveLen40(b *testing.B) {
+	src := "abaoboaobjaobjoajlafjgdsljfl;ajflasjd;lj"
+	dest := "da;ldfjaljdasl;fjalsdjflkadsjflajfhdiofj"
+
+	for i := 0; i < b.N; i++ {
+		CountLevNoRec(src, dest)
+	}
+
+}
+
 func BenchmarkRecursiveLen80(b *testing.B) {
 	src := "abaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjf"
 	dest := "da;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdaj"
@@ -23,3 +43,128 @@ func BenchmarkRecursiveLen240(b *testing.B) {
 
 }
 
+func BenchmarkCountDamNoRec10(b *testing.B) {
+	src := "abaoboaobj"
+	dest := "da;ldfjalj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamNoRec(src, dest)
+	}
+
+}
+
+func BenchmarkCountDamNoRec40(b *testing.B) {
+	src := "abaoboaobjaobjoajlafjgdsljfl;ajflasjd;lj"
+	dest := "da;ldfjaljdasl;fjalsdjflkadsjflajfhdiofj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamNoRec(src, dest)
+	}
+
+}
+
+func BenchmarkCountDamNoRec80(b *testing.B) {
+	src := "abaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjf"
+	dest := "da;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdaj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamNoRec(src, dest)
+	}
+
+}
+
+
+func BenchmarkCountDamNoRec240(b *testing.B) {
+	src := "abaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjfabaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjfabaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjf"
+	dest := "da;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdajda;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdajda;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdaj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamNoRec(src, dest)
+	}
+
+}
+
+func BenchmarkCountDamRecCacheLen10(b *testing.B) {
+	src := "abaoboaobj"
+	dest := "da;ldfjalj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamRecCache(src, dest)
+	}
+
+}
+
+func BenchmarCountDamRecCacheLen40(b *testing.B) {
+	src := "abaoboaobjaobjoajlafjgdsljfl;ajflasjd;lj"
+	dest := "da;ldfjaljdasl;fjalsdjflkadsjflajfhdiofj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamRecCache(src, dest)
+	}
+
+}
+
+func BenchmarkCountDamRecCacheLen80(b *testing.B) {
+	src := "abaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjf"
+	dest := "da;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdaj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamRecCache(src, dest)
+	}
+
+}
+
+
+func BenchmarkCountDamRecCacheLen240(b *testing.B) {
+	src := "abaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjfabaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjfabaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjf"
+	dest := "da;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdajda;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdajda;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdaj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamRecCache(src, dest)
+	}
+
+}
+
+func BenchmarkCountDamRecNoCacheLen10(b *testing.B) {
+	src := "abaoboaobj"
+	dest := "da;ldfjalj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamRecNoCache(src, dest)
+	}
+
+}
+
+/*
+
+func BenchmarkCountDamRecNoCacheLen20(b *testing.B) {
+	src := "abaoboaobjaobjoajlaf"
+	dest := "da;ldfjaljdasl;fjals"
+
+	for i := 0; i < b.N; i++ {
+		CountDamRecNoCache(src, dest)
+	}
+
+}
+
+func BenchmarkCountDamRecNoCacheLen80(b *testing.B) {
+	src := "abaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjf"
+	dest := "da;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdaj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamRecNoCache(src, dest)
+	}
+
+}
+
+
+func BenchmarkCountDamRecNoCacheLen240(b *testing.B) {
+	src := "abaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjfabaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjfabaoboaobjaobjoajlafjgdsljfl;ajflasjd;ljdasf;ladsj;ldfsajkdasf;ldsfal;dfsjasdkjf"
+	dest := "da;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdajda;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdajda;ldfjaljdasl;fjalsdjflkadsjflajfhdiofjoijdsodfajodasjodsafjsadoiasdjdzpoijsdaj"
+
+	for i := 0; i < b.N; i++ {
+		CountDamRecNoCache(src, dest)
+	}
+
+}
+*/
--- lab-01/levenstein/unit_test.go
+++ lab-01/levenstein/unit_test.go
@@ -31,3 +31,185 @@ func TestEmptyString(t *testing.T) {
 	}
 
 }
+
+func TestString1(t *testing.T) {
+	eth := 1
+	w1 := "book"
+	w2 := "bosk"
+	
+	res, mat := CountLevNoRec(w1, w2)
+
+	if (res != eth) {
+		t.Fatalf("ERROR(LevInter) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res, mat = CountDamNoRec(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecMemo) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res = CountDamRecNoCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecursive) : want %d, have %d\n", eth, res)
+	}
+
+	res = CountDamRecCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(DLevIter) : want %d, have %d\n", eth, res)
+	}
+
+}
+
+func TestString2(t *testing.T) {
+	eth := 2
+	w1 := "book"
+	w2 := "back"
+	
+	res, mat := CountLevNoRec(w1, w2)
+
+	if (res != eth) {
+		t.Fatalf("ERROR(LevInter) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res, mat = CountDamNoRec(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecMemo) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res = CountDamRecNoCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecursive) : want %d, have %d\n", eth, res)
+	}
+
+	res = CountDamRecCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(DLevIter) : want %d, have %d\n", eth, res)
+	}
+
+}
+
+func TestString3(t *testing.T) {
+	eth := 3
+	w1 := "book"
+	w2 := "bacc"
+	
+	res, mat := CountLevNoRec(w1, w2)
+
+	if (res != eth) {
+		t.Fatalf("ERROR(LevInter) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res, mat = CountDamNoRec(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecMemo) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res = CountDamRecNoCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecursive) : want %d, have %d\n", eth, res)
+	}
+
+	res = CountDamRecCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(DLevIter) : want %d, have %d\n", eth, res)
+	}
+
+}
+
+func TestString4(t *testing.T) {
+	eth := 4
+	w1 := "aboba"
+	w2 := "acacb"
+	
+	res, mat := CountLevNoRec(w1, w2)
+
+	if (res != eth) {
+		t.Fatalf("ERROR(LevInter) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res, mat = CountDamNoRec(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecMemo) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res = CountDamRecNoCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecursive) : want %d, have %d\n", eth, res)
+	}
+
+	res = CountDamRecCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(DLevIter) : want %d, have %d\n", eth, res)
+	}
+
+}
+
+func TestString21(t *testing.T) {
+	eth := 2
+	w1 := "дверь"
+	w2 := "деврь"
+	
+	res, mat := CountLevNoRec(w1, w2)
+
+	if (res != eth) {
+		t.Fatalf("ERROR(LevInter) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	eth = 1
+
+	res, mat = CountDamNoRec(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecMemo) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res = CountDamRecNoCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecursive) : want %d, have %d\n", eth, res)
+	}
+
+	res = CountDamRecCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(DLevIter) : want %d, have %d\n", eth, res)
+	}
+
+}
+
+func TestString0(t *testing.T) {
+	eth := 0
+	w1 := "дверь"
+	w2 := "дверь"
+	
+	res, mat := CountLevNoRec(w1, w2)
+
+	if (res != eth) {
+		t.Fatalf("ERROR(LevInter) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res, mat = CountDamNoRec(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecMemo) : want %d, have %d\n", eth, res)
+		mat.PrintMatrix()
+	}
+
+	res = CountDamRecNoCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(LevRecursive) : want %d, have %d\n", eth, res)
+	}
+
+	res = CountDamRecCache(w1, w2)
+	if (res != eth) {
+		t.Fatalf("ERROR(DLevIter) : want %d, have %d\n", eth, res)
+	}
+
+}
